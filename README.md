dc4f49b9f72a267f0d29cbe3e515e1341147bea8
西山　明伸 <a_nishiyama@septeni-original.co.jp>
2016-09-30 17:01:52 +0900

    現環境のコピー

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # dotfiles
+先にMacを前提としての構築
+
+# mac インストール
+```
+ sh ./tools/install.sh
+```
+
+
+## Macのアプリケーションインストール一部自動化
+
+### brew cask upgrade
+```
+for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done
+```
+### brew cask upgrade
+```
+brew cask search {パッケージ名}
+brew cask install {パッケージ名}
+brew cask uninstall {パッケージ名}
+```
+
+
+## anyenv 使い方
+nodeやscala,Javaのversionを複数管理する
+
+### Nodeインストール
+```
+$ ndenv install v0.11.13　←バージョンは適宜好きなものを！
+$ ndenv rehash　　　　　   ←再構築（インストール後のおまじない）
+$ ndenv global v0.11.13　 ←デフォルトのバージョンに設定
+$ node -v
+$ ndenv versions ←version確認
+  v0.11.14
+```
+
---
+++ tools/install_mac.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# mac 隠しファイル表示
+defaults write com.apple.finder AppleShowAllFiles -boolean true
+killall Finder
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install vim
+brew install zsh
+brew install git
+brew install curl
+
+# Mac Application installs
+brew tap caskroom/cask
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install google-japanese-ime
+brew cask install atom
+brew cask install intellij-idea
+brew cask install slack
+brew cask install dropbox
+brew cask install sequel-pro
+brew cask install sourcetree
+brew cask install gyazo
+brew cask install skype
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install 1password
+
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".idea" ]] && continue
+    [[ "$f" == ".gitmodules" ]] && continue
+    [[ "$f" == ".gitconfig" ]] && continue #手動で書き換え
+
+#    echo "${HOME}/.dotfiles/$f" "${HOME}/$f"
+    ln -sfn "${HOME}/.dotfiles/$f" $HOME/"$f"
+done
+
+
+anyenv install ndenv
+anyenv install jenv
+anyenv install scalaenv
+anyenv install sbtenv
+
+ndenv install v6.7.0
+scalaenv install scala-2.11.8
+sbtenv install sbt-0.13.8
+
+ndenv global v6.7.0
+scalaenv global scala-2.11.8
+sbtenv global sbt-0.13.9
+
+
