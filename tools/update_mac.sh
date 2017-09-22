9ed6f3d30f243f3d223ff9d402978220f5877d5a
ugdark <ugdark@gmail.com>
2017-09-22 22:11:10 +0900

    rbenvの更新する。

--- tools/update_mac.sh
+++ tools/update_mac.sh
@@ -3,3 +3,6 @@
 brew upgrade
 
 for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done
+
+# rbenv を更新 (よくわすれる)
+cd ~/.anyenv/envs/rbenv/plugins/ruby-build && git pull && cd -
